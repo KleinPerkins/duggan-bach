b2847da3177652225a47886e49fec621074a4dfb
Laurence Duggan <49759187+KleinPerkins@users.noreply.github.com>
2026-05-28 17:09:55 -1000

    Point embed at correct Form Responses tab

--- config.js
+++ config.js
@@ -24,6 +24,6 @@ window.SITE_CONFIG = {
 
   // Flight sheet — populated after manual Form-to-Sheet link step
   // Format: published-to-web URL of the sheet (set to null until linked)
-  flightSheetEmbedUrl: "https://docs.google.com/spreadsheets/d/1IA9tqeFJJlz_SrlwgWwftjI5ub8gs5uddGXdV733Nio/htmlembed?gid=1236950995&widget=true&headers=false&chrome=false",
-  flightSheetViewUrl: "https://docs.google.com/spreadsheets/d/1IA9tqeFJJlz_SrlwgWwftjI5ub8gs5uddGXdV733Nio/edit#gid=1236950995",
+  flightSheetEmbedUrl: "https://docs.google.com/spreadsheets/d/1IA9tqeFJJlz_SrlwgWwftjI5ub8gs5uddGXdV733Nio/htmlembed?gid=1113867018&widget=true&headers=false&chrome=false",
+  flightSheetViewUrl: "https://docs.google.com/spreadsheets/d/1IA9tqeFJJlz_SrlwgWwftjI5ub8gs5uddGXdV733Nio/edit#gid=1113867018",
 };
